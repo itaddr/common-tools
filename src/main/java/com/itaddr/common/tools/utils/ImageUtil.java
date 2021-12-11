9d1e1e337dc7c6d8cb5b60cdad1b061fe5855182
majiaqi <majiaqi@dongyou.com>
2021-12-11 10:33:39 +0800

    测试功能

---
+++ src/main/java/com/itaddr/common/tools/utils/ImageUtil.java
@@ -0,0 +1,17 @@
+package com.itaddr.common.tools.utils;
+
+/**
+ * @Title null.java
+ * @Package com.itaddr.common.tools.utils
+ * @Author 马嘉祺
+ * @Date 2021/11/25 15:51
+ * @Description
+ */
+public class ImageUtil {
+
+    public static boolean isImage() {
+
+        return false;
+    }
+
+}

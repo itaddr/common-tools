ebd432257cf4960895222167b04b3c0f1e3cfb0c
majiaqi <majiaqi@dongyou.com>
2022-06-15 20:46:39 +0800

    ThreadUtil线程中断处理

--- src/main/java/com/itaddr/common/tools/utils/ThreadUtil.java
+++ src/main/java/com/itaddr/common/tools/utils/ThreadUtil.java
@@ -18,7 +18,6 @@ import com.itaddr.common.tools.exception.NotCaptureException;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.locks.LockSupport;
 
 /**
  * @Author 马嘉祺
@@ -26,28 +25,28 @@ import java.util.concurrent.locks.LockSupport;
  * @Description <p></p>
  */
 public final class ThreadUtil {
-    
+
     public static final Predicate1 DEFAULT_AWAKE = () -> false;
-    
+
     private ThreadUtil() {
     }
-    
+
     public static ThreadFactory factory(String threadNamePrefix) {
         return new ThreadFactory() {
             private AtomicInteger threadNum = new AtomicInteger(0);
-            
+
             @Override
             public Thread newThread(Runnable r) {
                 return new Thread(r, threadNamePrefix + threadNum.getAndIncrement());
             }
         };
     }
-    
+
     public static void sleep(long timeout, TimeUnit unit) throws InterruptedException {
         long remaining = unit.toNanos(timeout);
         Thread.sleep(remaining / 1000000L, (int) (remaining % 1000000L));
     }
-    
+
     public static void sleepUninterruptibly(long timeout, TimeUnit unit) {
         long remaining = unit.toNanos(timeout);
         long beginTimeNanos = System.nanoTime(), endTimeNanos;
@@ -62,14 +61,14 @@ public final class ThreadUtil {
             }
         }
     }
-    
+
     public static void join(Thread thread) throws InterruptedException {
         if (null == thread || !thread.isAlive()) {
             return;
         }
         thread.join();
     }
-    
+
     public static void join(Thread thread, long timeout, TimeUnit unit) throws InterruptedException {
         if (null == thread || !thread.isAlive()) {
             return;
@@ -77,47 +76,38 @@ public final class ThreadUtil {
         long remaining = unit.toNanos(timeout);
         thread.join(remaining / 1000000L, (int) (remaining % 1000000L));
     }
-    
+
     public static void joinUninterruptibly(Thread thread) {
         if (null == thread || !thread.isAlive()) {
             return;
         }
-        boolean isInterrupted = false;
         while (thread.isAlive()) {
             try {
                 thread.join();
-                if (!thread.isInterrupted() && isInterrupted) {
-                    thread.interrupt();
-                }
-                return;
             } catch (InterruptedException ignored) {
-                isInterrupted = true;
+                Thread.currentThread().interrupt();
             }
         }
     }
-    
+
     public static void joinUninterruptibly(Thread thread, long timeout, TimeUnit unit) {
         if (null == thread || !thread.isAlive()) {
             return;
         }
-        boolean isInterrupted = false;
         long remaining = unit.toNanos(timeout);
         long beginTimeNanos = System.nanoTime(), endTimeNanos;
         while (thread.isAlive() && remaining > 0) {
             try {
                 thread.join(remaining / 1000000L, (int) (remaining % 1000000L));
-                if (!thread.isInterrupted() && isInterrupted) {
-                    thread.interrupt();
-                }
                 return;
             } catch (InterruptedException ignored) {
+                Thread.currentThread().interrupt();
                 remaining -= (endTimeNanos = System.nanoTime()) - beginTimeNanos;
                 beginTimeNanos = endTimeNanos;
-                isInterrupted = true;
             }
         }
     }
-    
+
     public static void uninterruptibly(Predicate1 awake, Consumer1 await) {
         if (null == awake || null == await) {
             throw new NullPointerException();
@@ -127,12 +117,13 @@ public final class ThreadUtil {
                 await.accept();
                 return;
             } catch (InterruptedException ignored) {
+                Thread.currentThread().interrupt();
             } catch (Throwable e) {
                 throw new NotCaptureException(e);
             }
         }
     }
-    
+
     public static void uninterruptibly(Predicate1 awake, Consumer2 await, long timeoutMs) {
         if (null == await) {
             throw new IllegalArgumentException("await不能为空");
@@ -145,6 +136,7 @@ public final class ThreadUtil {
                 await.accept(remaining / 1000000L);
                 return;
             } catch (InterruptedException ignored) {
+                Thread.currentThread().interrupt();
                 remaining -= (endTimeNanos = System.nanoTime()) - beginTimeNanos;
                 beginTimeNanos = endTimeNanos;
             } catch (Throwable e) {
@@ -152,7 +144,7 @@ public final class ThreadUtil {
             }
         }
     }
-    
+
     public static void uninterruptibly(Predicate1 awake, Consumer3 await, long timeoutMs, int nanos) {
         if (null == await) {
             throw new IllegalArgumentException("await不能为空");
@@ -165,6 +157,7 @@ public final class ThreadUtil {
                 await.accept(remaining / 1000000L, (int) (remaining % 1000000L));
                 return;
             } catch (InterruptedException ignored) {
+                Thread.currentThread().interrupt();
                 remaining -= (endTimeNanos = System.nanoTime()) - beginTimeNanos;
                 beginTimeNanos = endTimeNanos;
             } catch (Throwable e) {
@@ -172,7 +165,7 @@ public final class ThreadUtil {
             }
         }
     }
-    
+
     public static void uninterruptibly(Predicate1 awake, Consumer4 await, long timeout, TimeUnit unit) {
         if (null == await) {
             throw new IllegalArgumentException("await不能为空");
@@ -185,6 +178,7 @@ public final class ThreadUtil {
                 await.accept(remaining, TimeUnit.NANOSECONDS);
                 return;
             } catch (InterruptedException ignored) {
+                Thread.currentThread().interrupt();
                 remaining -= (endTimeNanos = System.nanoTime()) - beginTimeNanos;
                 beginTimeNanos = endTimeNanos;
             } catch (Throwable e) {
@@ -192,25 +186,25 @@ public final class ThreadUtil {
             }
         }
     }
-    
+
     public interface Consumer1 {
         void accept() throws InterruptedException;
     }
-    
+
     public interface Consumer2 {
         void accept(long timeoutMs) throws InterruptedException;
     }
-    
+
     public interface Consumer3 {
         void accept(long timeoutMs, int nanos) throws InterruptedException;
     }
-    
+
     public interface Consumer4 {
         void accept(long timeout, TimeUnit unit) throws InterruptedException;
     }
-    
+
     public interface Predicate1 {
         boolean test();
     }
-    
+
 }
